6a9d97c7849b4c560d321ce4cbeaeb482f18d804
Akshansh <akshansh@users.noreply.github.com>
2019-03-12 22:55:35 -0400

    Turtle Graphics Creator
    Using Custom Turtle Graphics and p5.js, I have created a user interface for generating custom shapes.
    Click on Forward, Backward, Left, Right, and Draw buttons to create faces and other objects.

---
+++ turtleGraphicsCreator/logic.md
@@ -0,0 +1,40 @@
+# Turtle Graphic Creator
+
+* If checkbox is checked, allow drawing
+* If button is pressed, move a few steps in that direction
+
+## Draw Face Steps
+
+* DON'T DRAW
+  * Back: 6
+  * Left
+  * Forward: 5
+* DRAW
+  * Right
+  * Forward: 10
+  * Right
+  * Forward: 10
+  * Right
+  * Forward: 10
+  * Right
+  * Forward: 10
+* DON'T DRAW
+  * Back: 4
+  * Right
+  * Forward
+* DRAW
+  * Draw Eye
+* DON'T DRAW
+  * Forward: 5
+* DRAW
+  * Draw Eye
+* DON'T DRAW
+  * Forward: 2
+  * Right
+  * Forward: 2
+* DRAW
+  * Forward
+  * Right
+  * Forward: 8
+  * Right: 2
+  * Forward: 8
---
+++ turtleGraphicsCreator/turtleGraphicCreator.js
@@ -0,0 +1,106 @@
+// Program to create a moving car using Turtle graphics
+
+// require https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.14/p5.js
+// require /turtles/turtle/turtle.js
+
+// For logic and updated tutle file, check GitHub
+// Link: 
+
+
+var myTurtle;
+
+// Customization with Parameters
+var moveAmount;
+var turtleMoveForward_button;
+var turtleMoveReverse_button;
+var turtleMoveLeft_button;
+var turtleMoveRight_button;
+var turtlePenDown_checkbox;
+
+var turtleDrawEyes_button;
+
+
+function setup() {
+    createCanvas(500, 500);
+    myTurtle = new Turtle();
+    background(0);
+
+    stroke(255);
+    strokeColor = 255;
+    strokeColorChangeRate = 40;
+
+    // Draw checkbox
+    turtlePenDown_checkbox = createCheckbox('Draw', true);
+    turtlePenDown_checkbox.changed(drawTurtle);
+    createP();
+
+    moveAmount = 30;
+
+    // Movement buttons
+    turtleMoveForward_button = createButton('Forward');
+    turtleMoveForward_button.mousePressed(moveFwd);
+    turtleMoveForward_button = createButton('Backward');
+    turtleMoveForward_button.mousePressed(moveBwd);
+    createP();
+    turtleMoveForward_button = createButton('Left');
+    turtleMoveForward_button.mousePressed(moveLeft);
+    turtleMoveForward_button = createButton('Right');
+    turtleMoveForward_button.mousePressed(moveRight);
+    createP();
+    turtleDrawEyes_button = createButton('Draw Eye');
+    turtleDrawEyes_button.mousePressed(drawEye);
+
+}
+
+function draw() {
+
+    noFill();
+    stroke(strokeColor);
+    strokeWeight(2);
+
+    // noLoop();
+}
+
+
+function drawTurtle() {
+    if (this.checked()) {
+        myTurtle.penDown();
+        console.log("Draw is on.");
+    } else {
+        myTurtle.penUp();
+        console.log("Cannot draw.");
+    }
+}
+
+
+function moveFwd() {
+    myTurtle.moveForward(moveAmount);
+}
+
+function moveBwd() {
+    myTurtle.moveBackward(moveAmount);
+}
+
+function moveRight() {
+    myTurtle.turnRight(90);
+    myTurtle.moveForward(moveAmount);
+}
+
+function moveLeft() {
+    myTurtle.turnLeft(90);
+    myTurtle.moveForward(moveAmount);
+}
+
+function drawEye() {
+
+    // draw eye
+    for (var i = 0; i < 30; i++) {
+        myTurtle.moveForward(10);
+        myTurtle.penUp();
+        myTurtle.moveForward(40);
+        myTurtle.penDown();
+        myTurtle.turnRight(129);
+    }
+    myTurtle.moveForward(37);
+    myTurtle.turnRight(90);
+}
